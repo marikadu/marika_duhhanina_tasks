566b198c2fc5be42d6283b5d545f40ac6795c5e1
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-01 11:16:57 +0200

    added 3.5

--- ex1.php
+++ ex1.php
@@ -26,10 +26,47 @@ echo "My name is \"David\"";
 <h1>3.3 Current date.</h1>
 <?php echo date("d.m.y")?>
 
+<?php
+/* Other way to complete 3.4
 <?php // 3.4?>
 <?php $title = "PHP is interesting"?>
 <h1>3.4 <?php echo $title?> </h>
+*/
+?>
 
+<?php
+$title1 = "PHP is interesting";
+echo "<h1>" . $title1 . "</h1>";
+?>
+
+<?php // 3.5?>
+<h1>3.5 Table.</h1>
+<?php 
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+echo "
+<table>
+
+<tr>
+<th> S.N </th><th> Name </th><th> Grade </th>
+</tr>
+
+<tr>
+<td> 1 </th><th> Pekka </th><th> $g1 </td>
+</tr>
+
+<tr>
+<td> 2 </th><th> Johanna </th><th> $g2 </td>
+</tr>
+
+<tr>
+<td> 3 </th><th> John </th><th> $g3 </td>
+</tr>
+
+
+</table>";
+?>
 
 </body>
 </html>

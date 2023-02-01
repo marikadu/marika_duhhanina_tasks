dfa016c975a49d4531ed68e0e4add37c676d81f1
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-01 10:40:18 +0200

    Initial commit

---
+++ ex1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php page</title>
+</head>
+<body>
+    <h1>3.1 Write a simple PHP script to print your information (Name and your groupid).</h1>
+
+<?php
+echo "Marika Duhhanina <br>";
+echo "BBCAP22";
+?>
+
+<h1>3.2 Write PHP code to display the following message.</h1>
+
+<?php
+echo "Hello World!<br>";
+echo "My name is \"David\"";
+?>
+
+<h1>3.3 Current date. </h1>
+
+<?php echo date("d.m.y")?>
+
+</body>
+</html>

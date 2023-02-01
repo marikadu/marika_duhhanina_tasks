d1a20ff30915301fce236d82b687d379630e8c4e
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-01 10:55:21 +0200

    added layout files that we did in class

---
+++ layout/aboutus.php
@@ -0,0 +1,7 @@
+<?php
+$title = "About Us";
+include "header.php"?>
+
+<h1> We are something </h1>
+
+<?php include "footer.php"?>
---
+++ layout/contactus.php
@@ -0,0 +1,7 @@
+<?php 
+$title = "Contact Us";
+include "header.php"?>
+
+<h1> Mail: company@company.com </h1>
+
+<?php include "footer.php"?>

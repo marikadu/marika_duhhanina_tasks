d37393f06c7e86caa92d9bffbd3c91c16b0e72a8
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-01 13:46:54 +0200

    making webpage php

---
+++ layout_for_ex2/tops.php
@@ -0,0 +1,75 @@
+<?php
+$title = "Cecile | Tops";
+include "header.php"?>
+
+
+<div class="container">
+
+<div class="p-5 text-center bg-image">
+        <div class="mask" style="background-color: #ffe8e0d5">
+          <div class="d-flex justify-content-center align-items-center h-100">
+            <div class="text-white">
+              <h1 style="padding-top: 16px; padding-bottom: 16px;">Tops</h1>
+              <p class="card-text">Wear exclusive tops</p>
+
+            </div>
+
+            
+          </div>
+        </div>
+  </div>
+
+
+  
+            <div class="row">
+              <div class="col-lg-4 col-md-6 col-sm-4" style="float: left;  margin-left: 100px;"  >
+                  <div class="product-grid">
+                      <div class="product-image">
+
+                          
+
+                          <a href="405.php" class="image">
+                              <img src="images/model2.2.png" alt="Hoodie">
+
+                          </a>
+                          <a href="404.html" class="add-to-cart">OUT OF STOCK</a>
+                      </div>
+                      <div class="product-content">
+                          <h3 class="title"><a href="405.php">Hoodie</a></h3>
+                          <div class="price">60.00€</div>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 col-sm-4" style="float: right;  margin-right: 100px;" >
+                <div class="product-grid">
+                    <div class="product-image">
+                        <a href="405.html" class="image">
+
+                            
+
+                            <img src="images/model4.11.png" alt="Sweater">
+
+                        </a>
+                        
+                        <a href="405.php" class="add-to-cart">OUT OF STOCK</a>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title"><a href="405.php">Sweater</a></h3>
+                        <div class="price">49.00€ <span>70.00€</span></div>
+                        
+                    </div>
+                </div>
+            </div>
+        
+          </div>
+          
+
+
+
+
+    </header>
+
+    
+
+<?php include "footer.php"?>

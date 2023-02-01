34030217487de824ab458208c0a806a13ea51e17
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-01 10:50:48 +0200

    added 3.4

--- ex1.php
+++ ex1.php
@@ -7,25 +7,29 @@
     <title>My first php page</title>
 </head>
 <body>
-    <h1>3.1 Write a simple PHP script to print your information (Name and your groupid).</h1>
 
+<?php // 3.1?>
+<h1>3.1 Write a simple PHP script to print your information (Name and your groupid).</h1>
 <?php
 echo "Marika Duhhanina <br>";
 echo "BBCAP22";
 ?>
 
+<?php // 3.2?>
 <h1>3.2 Write PHP code to display the following message.</h1>
-
 <?php
 echo "Hello World!<br>";
 echo "My name is \"David\"";
 ?>
 
+<?php // 3.3?>
 <h1>3.3 Current date.</h1>
-
 <?php echo date("d.m.y")?>
 
-<h1>  </h>
+<?php // 3.4?>
+<?php $title = "PHP is interesting"?>
+<h1>3.4 <?php echo $title?> </h>
+
 
 </body>
 </html>

4362af6799f2c054d7cc3a2c2e022bfd5ddd604e
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-14 11:01:05 +0200

    added 2 buttoms

--- crud1/updatesingle.php
+++ crud1/updatesingle.php
@@ -9,16 +9,17 @@ $row = mysqli_fetch_array($result);
 
 <h2> Update your information below </h2>
 <form name = "update" method = "post" action="">
-    <input type="text" name="fname" placeholder="First Name" required value="<?php echo $row['fname'];?>"> <br>
-    <input type="text" name="lname" placeholder="Last Name" required value="<?php echo $row['lname'];?>"> <br>
-    <input type="text" name="city" placeholder="City" required value="<?php echo $row['city'];?>"> <br>
-    <select name="groupid" value="<?php echo $row['groupid'];?>"> <br>>
+    <input type="text" name="fname" placeholder="First Name" required value="<?php echo $row['fname'];?>"> <br><br>
+    <input type="text" name="lname" placeholder="Last Name" required value="<?php echo $row['lname'];?>"> <br><br>
+    <input type="text" name="city" placeholder="City" required value="<?php echo $row['city'];?>"> <br><br>
+    <select name="groupid" value="<?php echo $row['groupid'];?>"> <br><br>
         <option value="BBCAP22"> BBCAP22 </option>
         <option value="BBCAP23"> BBCAP23 </option>
         <option value="BBCAP24"> BBCAP24 </option>
         <option value="BBCAP24"> Others </option>
     </select>
-    <input type="submit" value="Submit" name="submit">
+    <input type="update" value="Update Your Info" name="submit"><br><br>
+    <input type="delete" value="Delete Your Info" name="submit"><br><br>
 
 </form>
 
